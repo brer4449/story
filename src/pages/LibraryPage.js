819979aecaf8e97332af4df4a53b46926caa61af
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-12-13 11:43:47 -0500

    library page under construction

--- src/pages/LibraryPage.js
+++ src/pages/LibraryPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Footer from "../components/Footer/index";
 
+const headerStyle = {
+  color: "red",
+};
 function Library() {
   return (
     <div>
@@ -9,10 +12,18 @@ function Library() {
           <div className="col-12">
             <div className="card text-center">
               <div className="card-header">
-                <h1>Search Our Library for a Story that Speaks to You</h1>
+                <h1 style={headerStyle}>
+                  The Library Page is Currently Under Construction
+                </h1>
+                {/* <h1>Search Our Library for a Story that Speaks to You</h1> */}
               </div>
               <div className="card-body">
-                <h1>This is the Library page</h1>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/construction.jpg`}
+                  width="400px"
+                  height="300px"
+                  alt="under construction"
+                />
               </div>
             </div>
           </div>
