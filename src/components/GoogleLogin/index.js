6b7a605207f5cb4ee00b1b7a4db2c2ba184e1c00
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-12-27 21:28:20 -0500

    added refresh token

--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
+// refresh token
+import { refreshTokenSetup } from "../../utils/refreshtoken";
 
 // client id: 402619144358-spoalto8jcjv4n3mqou831qfvq4e7c1h.apps.googleusercontent.com
 // client secret: onlUPfO5YE_uXPgUSkk0x1hw
@@ -9,6 +11,10 @@ const clientId =
 function GoogleLoginComp() {
   const onSuccess = (res) => {
     console.log("[Login Success] currentUser: ", res.profileObj);
+    console.log(res.profileObj.imageUrl);
+
+    // initializing the setup
+    refreshTokenSetup(res);
   };
   const onFailure = (res) => {
     console.log("[Login failed] res: ", res);
