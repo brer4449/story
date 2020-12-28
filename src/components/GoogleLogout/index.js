c10d3ca773cf18d6ae2dc5fdbbe117111518bc36
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-12-27 20:44:50 -0500

    added client id's

--- src/components/GoogleLogout/index.js
+++ src/components/GoogleLogout/index.js
@@ -3,10 +3,11 @@ import { GoogleLogout } from "react-google-login";
 
 // client id: 402619144358-spoalto8jcjv4n3mqou831qfvq4e7c1h.apps.googleusercontent.com
 // client secret: onlUPfO5YE_uXPgUSkk0x1hw
-const clientId = "YOUR_CLIENT_ID.apps.googleusercontent.com";
+const clientId =
+  "402619144358-spoalto8jcjv4n3mqou831qfvq4e7c1h.apps.googleusercontent.com";
 
 function GoogleLogoutComp() {
-  const onSuccess = (res) => {
+  const onSuccess = () => {
     alert("Logout made successfully");
   };
 
