a585ec2c581b60cff6c032561d32179321278a12
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-12-29 18:19:26 -0500

    cleaned up footer, gave options for backround color

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./style.css";
-// import SocialFollow from "../Social/social";
 
 function Footer() {
   return (
     <div>
       <footer className="footer">
-        <span id="bidbot">
-          StoriesTold, LLC ©2020
-          {/* <SocialFollow /> */}
-        </span>
+        <span>StoriesTold, LLC ©2020</span>
       </footer>
     </div>
   );
