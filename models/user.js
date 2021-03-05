296de946e6c3cd03c95c59b86105e0364175e549
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-05 09:33:29 -0500

    updated user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,32 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  timestamps: true,
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// before we save want to encrypt password by adding middleware
+userSchema.pre("save", async function (next) {
+  // this check prevents a new salt/hash from being generated if we modify only the email or name and NOT the password
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
