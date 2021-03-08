6aa8ba22f10207557932b98df5feebdb790cdf0e
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-08 15:27:41 -0500

    altered setInputs to better reflect state, set up for error handling (currently not updating state)

--- client/src/pages/GiftPage.js
+++ client/src/pages/GiftPage.js
@@ -5,13 +5,29 @@ import API from "../utils/API";
 import Footer from "../components/Footer/index";
 
 function Gift() {
+  const form = document.FC;
   const [inputs, setInputs] = useState({
-    timeFrame: "",
-    recipient: "",
-    priceRange: "",
-    genre: "",
-    size: "",
-    specifics: "",
+    recipient: { myself: false, family: false, friend: false, other: false },
+    type: {
+      animation: false,
+      video: false,
+      audio: false,
+      song: false,
+      written: false,
+      unsureType: false,
+    },
+    storyteller: {
+      me: false,
+      giftee: false,
+      someoneElse: false,
+      unsureStoryTeller: false,
+    },
+    timeFrame: {
+      oneMo: false,
+      twoMo: false,
+      threeMo: false,
+      unsureTimeFrame: false,
+    },
   });
   // Server state handling
   const [serverState, setServerState] = useState({
@@ -31,6 +47,46 @@ function Gift() {
     specifics: (val) => !!val,
   };
 
+  // My own attempt at validation rules
+  // https://stackoverflow.com/questions/11787665/making-sure-at-least-one-checkbox-is-checked
+  // TODO: set the field error for that question block
+  const validateAtLeastOneChecked = function () {
+    if (
+      !inputs.recipient.myself ||
+      !inputs.recipient.family ||
+      !inputs.recipient.friend ||
+      !inputs.recipient.other
+    ) {
+      console.log("RECIPIENTS ALL FALSE");
+    }
+    if (
+      !inputs.type.animation ||
+      !inputs.type.video ||
+      !inputs.type.audio ||
+      !inputs.type.song ||
+      !inputs.type.written ||
+      !inputs.type.unsureType
+    ) {
+      console.log("TYPES ALL FALSE");
+    }
+    if (
+      !inputs.storyteller.me ||
+      !inputs.storyteller.giftee ||
+      !inputs.storyteller.someoneElse ||
+      !inputs.storyteller.unsureStoryTeller
+    ) {
+      console.log("STORYTELLERS ALL FALSE");
+    }
+    if (
+      !inputs.timeFrame.oneMo ||
+      !inputs.timeFrame.twoMo ||
+      !inputs.timeFrame.threeMo ||
+      !inputs.timeFrame.unsureTimeFrame
+    ) {
+      console.log("TIMEFRAMES ALL FALSE");
+    }
+  };
+
   // Validate function that updates state and returns true if all rules pass
   const validate = () => {
     let errors = {};
@@ -47,16 +103,22 @@ function Gift() {
   const renderFieldError = (field) => {
     if (fieldErrors[field]) {
       return (
-        <p className="errorMsg">Please enter a valid {field} and try again.</p>
+        // <p className="errorMsg">Please enter a valid {field} and try again.</p>
+        <p className="errorMsg">
+          Please check that an answer to questions about {field} has at least
+          one check and try again
+        </p>
       );
     }
   };
 
   const handleOnChange = (e) => {
     e.persist();
+    validateAtLeastOneChecked();
+    // console.log(e.target.id);
     setInputs((prev) => ({
       ...prev,
-      [e.target.id]: e.target.value,
+      [e.target.id]: e.target.checked,
     }));
   };
 
@@ -68,15 +130,15 @@ function Gift() {
     if (ok) {
       setFieldErrors({});
       setInputs({
-        timeFrame: "",
-        recipient: "",
-        priceRange: "",
-        genre: "",
-        size: "",
-        specifics: "",
+        // recipient: "",
+        // type: "",
+        // storyteller: "",
+        // timeFrame: "",
       });
     }
   };
+
+  // console.log(inputs.recipient.myself);
 
   useEffect(() => {
     // Only perform interactive validation after submit
@@ -100,7 +162,7 @@ function Gift() {
     //   .then((r) => {
     //     console.log(r)
     //   })
-    //   .catch((r) => {
+    //   .catch((err) => {
     //     console.error(err)
     //   });
     if (inputs) {
@@ -129,84 +191,7 @@ function Gift() {
                 <h1>Give a Gift That Never Gets Old!</h1>
               </div>
               <div className="card-body">
-                <div>
-                  <h1>Story Sample</h1>
-                  <small>
-                    by{" "}
-                    <em>
-                      <a href={"/Author"}>Author</a>
-                    </em>
-                  </small>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer eget mauris placerat odio rutrum hendrerit. Ut
-                    molestie tincidunt ex at dignissim. Nullam a leo massa. Sed
-                    finibus libero sed tellus porttitor, id maximus mauris
-                    egestas. Vivamus scelerisque dolor dolor, quis maximus ante
-                    pulvinar id. Mauris at augue efficitur arcu molestie mattis.
-                    Etiam vulputate ac mi nec eleifend. Proin in dignissim arcu,
-                    in pellentesque quam. Cras sodales ultrices tellus, quis
-                    pellentesque ligula consequat et. Etiam tempus odio ipsum,
-                    sed euismod dui tristique id. Suspendisse ut quam vel enim
-                    rutrum aliquet.
-                  </p>
-                  <p>
-                    Vivamus ex turpis, porta sit amet dolor ut, molestie tempus
-                    risus. Suspendisse id erat in lectus tincidunt scelerisque
-                    nec eget dui. Quisque fringilla erat eu odio molestie
-                    efficitur. Proin eu vehicula purus. Nam egestas ac urna ac
-                    elementum. Donec a molestie lorem. Quisque consequat nunc
-                    ex, ut lobortis purus eleifend quis. Vestibulum tempus
-                    luctus auctor. Nulla semper diam a risus vestibulum cursus.
-                    Nulla rhoncus nisl mollis, molestie lorem et, hendrerit
-                    nibh. Praesent at tincidunt augue, nec sodales eros. Sed a
-                    bibendum justo.
-                  </p>
-                  <p>
-                    Phasellus eu tincidunt ante, scelerisque volutpat ex. Nunc
-                    nec turpis sit amet nibh fermentum hendrerit et non quam.
-                    Fusce sodales risus vel enim viverra efficitur. Pellentesque
-                    habitant morbi tristique senectus et netus et malesuada
-                    fames ac turpis egestas. Vestibulum consequat consectetur
-                    pretium. Etiam tempus blandit vehicula. Curabitur
-                    pellentesque elit quis faucibus convallis. Nam quis arcu ac
-                    felis aliquam accumsan in id massa. Fusce id eros vel lacus
-                    consequat hendrerit. Donec in cursus augue, nec convallis
-                    velit. Fusce at euismod leo, at pellentesque dui. Mauris
-                    egestas ornare sapien, id vestibulum est convallis vitae.
-                    Donec vestibulum urna at mattis facilisis. Pellentesque
-                    mattis quis neque sed posuere.
-                  </p>
-                  <p>
-                    Integer finibus lorem et suscipit fermentum. Donec quis
-                    venenatis velit. Quisque sodales, eros eu porttitor aliquam,
-                    enim lorem varius enim, ac faucibus nulla justo ut nisl. Ut
-                    efficitur at lacus vel porta. Donec fringilla nibh a lacus
-                    scelerisque vestibulum. Vivamus eros risus, dapibus
-                    scelerisque risus sed, commodo fermentum quam. Aliquam
-                    consequat est mauris, vel tristique arcu tempor tempor.
-                    Aenean a posuere mi, at rhoncus diam. Suspendisse ultricies
-                    massa metus, in vulputate augue pharetra ac. Aenean
-                    convallis lacus magna, sed varius lorem vestibulum vel. Nam
-                    vehicula pretium condimentum.
-                  </p>
-
-                  <p>
-                    Pellentesque sed nisl pretium, venenatis turpis nec,
-                    faucibus lectus. Orci varius natoque penatibus et magnis dis
-                    parturient montes, nascetur ridiculus mus. Pellentesque
-                    habitant morbi tristique senectus et netus et malesuada
-                    fames ac turpis egestas. Pellentesque est lacus, tristique
-                    vitae lorem id, congue ultrices enim. Nulla sem odio, dictum
-                    nec congue vel, efficitur sit amet augue. Morbi sagittis
-                    vitae libero vitae laoreet. Sed elit tellus, elementum ac
-                    ante nec, cursus scelerisque odio. Morbi dignissim, libero
-                    consectetur sollicitudin ullamcorper, mi est sodales nibh,
-                    sit amet sodales velit libero sed lectus. Proin gravida
-                    magna nisi, sit amet viverra felis lacinia vitae. Ut ornare
-                    ex vitae felis rhoncus, at pellentesque metus condimentum.
-                  </p>
-                </div>
+                {/* SAMPLE STORY HERE */}
                 <br></br>
                 <h2>
                   To Have Your Story Told Like The One Above, Simply Fill Out an
@@ -216,7 +201,7 @@ function Gift() {
                 <br></br>
                 <br></br>
                 <div>
-                  <Form onSubmit={handleSubmit}>
+                  <Form name="FC" onSubmit={handleSubmit}>
                     <div className="mb-10 py-3 py-3">
                       <h4>Who is this for?</h4>
                       <div>
@@ -227,6 +212,7 @@ function Gift() {
                           id="myself"
                           name="myself"
                           value="myself"
+                          onChange={handleOnChange}
                         ></input>
                       </div>
                       <div>
@@ -237,6 +223,7 @@ function Gift() {
                           id="family"
                           name="family"
                           value="family"
+                          onChange={handleOnChange}
                         ></input>
                       </div>
                       <div>
@@ -247,6 +234,7 @@ function Gift() {
                           id="friend"
                           name="friend"
                           value="friend"
+                          onChange={handleOnChange}
                         ></input>
                       </div>
                       <div>
@@ -313,13 +301,13 @@ function Gift() {
                         ></input>
                       </div>
                       <div>
-                        <label htmlFor="unsure">Not Sure</label>
+                        <label htmlFor="unsureType">Not Sure</label>
                         <input
                           style={{ margin: "8px" }}
                           type="checkbox"
-                          id="unsure"
-                          name="unsure"
-                          value="unsure"
+                          id="unsureType"
+                          name="unsureType"
+                          value="unsureType"
                         ></input>
                       </div>
                     </div>
@@ -346,23 +334,23 @@ function Gift() {
                         ></input>
                       </div>
                       <div>
-                        <label htmlFor="someone">Someone Else</label>
+                        <label htmlFor="someoneElse">Someone Else</label>
                         <input
                           style={{ margin: "8px" }}
                           type="checkbox"
-                          id="someone"
-                          name="someone"
-                          value="someone"
+                          id="someoneElse"
+                          name="someoneElse"
+                          value="someoneElse"
                         ></input>
                       </div>
                       <div>
-                        <label htmlFor="notSure">Not Sure</label>
+                        <label htmlFor="unsureStoryteller">Not Sure</label>
                         <input
                           style={{ margin: "8px" }}
                           type="checkbox"
-                          id="notSure"
-                          name="notSure"
-                          value="notSure"
+                          id="unsureStoryteller"
+                          name="unsureStoryteller"
+                          value="unsureStoryteller"
                         ></input>
                       </div>
                     </div>
@@ -399,13 +387,13 @@ function Gift() {
                         ></input>
                       </div>
                       <div>
-                        <label htmlFor="notSure">Not Sure</label>
+                        <label htmlFor="unsureTimeFrame">Not Sure</label>
                         <input
                           style={{ margin: "8px" }}
                           type="checkbox"
-                          id="notSure"
-                          name="notSure"
-                          value="notSure"
+                          id="unsureTimeFrame"
+                          name="unsureTimeFrame"
+                          value="unsureTimeFrame"
                         ></input>
                       </div>
                     </div>
