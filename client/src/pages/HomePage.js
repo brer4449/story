6712ea6219ec5babdb7e902bb1f1dad76b00cfae
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-16 16:43:40 -0400

    replaced carousel with video

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import Carousel from "react-bootstrap/Carousel";
 import Footer from "../components/Footer/index";
 // import axios from "axios";
 import API from "../utils/API";
@@ -52,56 +51,12 @@ function Home() {
                 <h3>Bringing Your Stories to Life</h3>
               </div>
               <div className="card-body">
-                <div className="carousel">
-                  {/* setting indicators to false removes default bottom dashes at the bottom of the gallery indicators={false}*/}
-                  <Carousel>
-                    <Carousel.Item>
-                      {/* process.env.PUBLIC_URL allows accessing  images from public folder */}
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/pic1.jpg`}
-                        height="500px"
-                        alt="toast and egg"
-                        width="500px"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/pic2.jpg`}
-                        height="500px"
-                        alt="empty airport"
-                        width="500px"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/pic3.jpg`}
-                        height="500px"
-                        alt="dead flowers"
-                        width="500px"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/pic4.jpg`}
-                        height="500px"
-                        alt="bath salts and flowers"
-                        width="500px"
-                      />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/pic5.jpg`}
-                        height="500px"
-                        alt="temple"
-                        width="500px"
-                      />
-                    </Carousel.Item>
-                  </Carousel>
-                </div>
-                <h4 className="text">
-                  Have a story that you want told by a professional? Stories
-                  Told is the place for you!
-                </h4>
+                <video height="500px" controls>
+                  <source
+                    src={`${process.env.PUBLIC_URL}/images/the_who.mp4`}
+                    type="video/mp4"
+                  />
+                </video>
               </div>
             </div>
           </div>
