a01556d22d5638e91da1c182af994cd8dd45b260
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-16 16:32:03 -0400

    got rid of alternate non-functioning seeding method

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -102,36 +102,3 @@ db.EmployeeForm.deleteMany({})
     console.error(err);
     process.exit(1);
   });
-
-// using multiple seed files (the below code) https://zellwk.com/blog/seed-database/
-// const fs = require("fs");
-// const util = require("util");
-// const readDir = util.promisify(fs.readdir).bind(fs);
-// const path = require("path");
-// const mongoose = require("mongoose");
-
-// function toTitleCase(str) {
-//   return str.replace(/\w\S*/g, (txt) => {
-//     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-//   });
-// }
-
-// console.log(readDir, "this is working");
-// async function seedDataBase(runSaveMiddleware = false) {
-//   const dir = await readDir(__dirname);
-//   const seedFiles = dir.filter((f) => f.endsWith(".seed.js"));
-//   for (const file of seedFiles) {
-//     const fileName = file.split(".seed.js")[0];
-//     const modelName = toTitleCase(fileName);
-//     const model = mongoose.models[modelName];
-//     // console.log(`Created seed for ${model}`);
-//     if (!model) throw new Error(`Cannot find Model ${modelName}`);
-//     const fileContents = require(path.join(__dirname, file));
-
-//     runSaveMiddleware
-//       ? await model.create(fileContents)
-//       : await model.insertMany(fileContents);
-//   }
-// }
-
-// exports.seedDataBase = seedDataBase;
