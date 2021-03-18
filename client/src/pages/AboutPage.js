0125afe7a068eb530f96ca9e7f90138cb223961f
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-18 18:11:53 -0400

    added bios and images

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -19,64 +19,122 @@ function About() {
             </p>
             <h1 className="text-center pt-5">Our Team</h1>
             <div className="row py-4">
+              <div className="col-2"></div>
               <div className="col-4">
                 <img
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
+                  alt="Head shot of Steven with mountains in the background"
+                  src={`${process.env.PUBLIC_URL}/images/steve.png`}
                 ></img>
               </div>
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-4">
+                <h3>Steven Kerr</h3>
+                <p className="text-left">
+                  Steven developed a deep appreciation for the power of a well
+                  told story from watching video essays on YouTube. He
+                  co-founded Stories Told in Portland Oregon and enjoys rock
+                  climbing, basketball and board games in his free time.
+                </p>
               </div>
+              <div className="col-2"></div>
             </div>
             <div className="row py-4">
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-2"></div>
+              <div className="col-4">
+                <h3>Brendan Erickson</h3>
+                <p className="text-right">
+                  Brendan has enjoyed story telling ever since he was a boy,
+                  sitting in his Dad's Ford Escort stuck in LA traffic as his
+                  pops would listen to This American Life. Still an avid
+                  listener today, Brendan paired with Steven to create fresh
+                  take on how stories can be composed. He also enjoys board
+                  games, and travel, when he's not developing some new website
+                  or app.
+                </p>
               </div>
               <div className="col-4">
                 <img
+                  alt="Head shot of Brendan"
                   src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
                 ></img>
               </div>
+              <div className="col-2"></div>
             </div>
             <div className="row py-4">
+              <div className="col-2"></div>
               <div className="col-4">
                 <img
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
+                  alt="Head shot of Imani"
+                  style={{ height: "310px", width: "300px" }}
+                  src={`${process.env.PUBLIC_URL}/images/imani.jpg`}
                 ></img>
               </div>
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-4">
+                <h3>Imani Wilson</h3>
+                <p className="text-left">
+                  Imani is a former 2011 New York Digital Film Academy Graduate
+                  with nearly half a decade’s experience in Film and Broadcast.
+                  After several years of working in local broadcast, Imani has
+                  worked on political campaigns, local and national
+                  advertisements, online content, film and co produced several
+                  successfully running podcast series.
+                </p>
               </div>
+              <div className="col-2"></div>
             </div>
             <div className="row py-4">
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-2"></div>
+              <div className="col-4">
+                <h3>Joseph Schmidt</h3>
+                <p className="text-right">
+                  Joseph has always been fascinated by storytelling. Joseph is
+                  currently honing his craft in South Dakota. When not writing
+                  he enjoys induging in film, is a tech nerd, and loves to read.
+                </p>
               </div>
               <div className="col-4">
                 <img
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
+                  alt="Head shot of Joseph"
+                  style={{ height: "310px", width: "180px" }}
+                  src={`${process.env.PUBLIC_URL}/images/joseph.jpg`}
                 ></img>
               </div>
+              <div className="col-2"></div>
             </div>
             <div className="row py-4">
+              <div className="col-2"></div>
               <div className="col-4">
                 <img
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
+                  alt="Head shot of Carolani"
+                  style={{ height: "310px", width: "220px" }}
+                  src={`${process.env.PUBLIC_URL}/images/carolani.jpg`}
                 ></img>
               </div>
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-4">
+                <h3>Carolani Bartell</h3>
+                <p className="text-left">
+                  Carolani is a writer from the DMV area. She has been working
+                  in the entertainment industry since childhood, recently
+                  writing for video games, internet content, and wonderful
+                  projects like these. She also writes and edits fiction, music,
+                  and screenplays, finding inspiration in just about any
+                  creative outlet. Her passion for storytelling is often
+                  accompanied by early mornings, a record player, and iced
+                  coffee.
+                </p>
               </div>
+              <div className="col-2"></div>
             </div>
             <div className="row py-4">
-              <div className="col-8">
-                <h1>TEXT</h1>
+              <div className="col-2"></div>
+              <div className="col-4">
+                <h3>Sean</h3>
               </div>
               <div className="col-4">
                 <img
                   src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
                 ></img>
               </div>
+              <div className="col-2"></div>
             </div>
           </div>
         </div>
