cffd93d369d8d0e4603fff625fb1ce257b4fab61
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-19 15:02:48 -0400

    restyled image staggering and added titles to artists

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -8,7 +8,10 @@ function About() {
         <div className="card text-center">
           <div className="card-body">
             <h1 className="pt-5">About Stories Told</h1>
-            <p className="p-5 mx-5">
+            <p
+              className="mx-5"
+              style={{ paddingLeft: "13%", paddingRight: "13%" }}
+            >
               Storytelling is such an important part of connecting with the
               people we love. These memories are what keeps friends and family
               together, but storytelling is actually really hard. When we tell
@@ -21,12 +24,6 @@ function About() {
             <div className="row py-4">
               <div className="col-2"></div>
               <div className="col-4">
-                <img
-                  alt="Head shot of Steven with mountains in the background"
-                  src={`${process.env.PUBLIC_URL}/images/steve.png`}
-                ></img>
-              </div>
-              <div className="col-4">
                 <h3>Steven Kerr</h3>
                 <p className="text-left">
                   Steven developed a deep appreciation for the power of a well
@@ -35,10 +32,22 @@ function About() {
                   climbing, basketball and board games in his free time.
                 </p>
               </div>
+              <div className="col-4">
+                <img
+                  alt="Head shot of Steven with mountains in the background"
+                  src={`${process.env.PUBLIC_URL}/images/steve.png`}
+                ></img>
+              </div>
               <div className="col-2"></div>
             </div>
             <div className="row py-4">
               <div className="col-2"></div>
+              <div className="col-4">
+                <img
+                  alt="Head shot of Brendan"
+                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
+                ></img>
+              </div>
               <div className="col-4">
                 <h3>Brendan Erickson</h3>
                 <p className="text-right">
@@ -51,14 +60,10 @@ function About() {
                   or app.
                 </p>
               </div>
-              <div className="col-4">
-                <img
-                  alt="Head shot of Brendan"
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
-                ></img>
-              </div>
               <div className="col-2"></div>
             </div>
+            <h1 className="py-3">The Who Story Artists</h1>
+            <h2 className="py-3">Multi Media Story</h2>
             <div className="row py-4">
               <div className="col-2"></div>
               <div className="col-4">
@@ -69,7 +74,7 @@ function About() {
                 ></img>
               </div>
               <div className="col-4">
-                <h3>Imani Wilson</h3>
+                <h3>Imani Wilson - Voice Talent</h3>
                 <p className="text-left">
                   Imani is a former 2011 New York Digital Film Academy Graduate
                   with nearly half a decade’s experience in Film and Broadcast.
@@ -81,67 +86,68 @@ function About() {
               </div>
               <div className="col-2"></div>
             </div>
-            <div className="row py-4">
-              <div className="col-2"></div>
-              <div className="col-4">
-                <h3>Joseph Schmidt</h3>
-                <p className="text-right">
-                  Joseph has always been fascinated by storytelling. Joseph is
-                  currently honing his craft in South Dakota. When not writing
-                  he enjoys induging in film, is a tech nerd, and loves to read.
-                </p>
-              </div>
-              <div className="col-4">
-                <img
-                  alt="Head shot of Joseph"
-                  style={{ height: "310px", width: "180px" }}
-                  src={`${process.env.PUBLIC_URL}/images/joseph.jpg`}
-                ></img>
-              </div>
-              <div className="col-2"></div>
+            <div className="col-2"></div>
+          </div>
+          <div className="row py-4">
+            <div className="col-2"></div>
+            <div className="col-4">
+              <h3>Sean - Animation</h3>
             </div>
-            <div className="row py-4">
-              <div className="col-2"></div>
-              <div className="col-4">
-                <img
-                  alt="Head shot of Carolani"
-                  style={{ height: "310px", width: "220px" }}
-                  src={`${process.env.PUBLIC_URL}/images/carolani.jpg`}
-                ></img>
-              </div>
-              <div className="col-4">
-                <h3>Carolani Bartell</h3>
-                <p className="text-left">
-                  Carolani is a writer from the DMV area. She has been working
-                  in the entertainment industry since childhood, recently
-                  writing for video games, internet content, and wonderful
-                  projects like these. She also writes and edits fiction, music,
-                  and screenplays, finding inspiration in just about any
-                  creative outlet. Her passion for storytelling is often
-                  accompanied by early mornings, a record player, and iced
-                  coffee.
-                </p>
-              </div>
-              <div className="col-2"></div>
+            <div className="col-4">
+              <img
+                alt="Head shot of Sean"
+                src={`${process.env.PUBLIC_URL}/images/sean.jpg`}
+              ></img>
             </div>
-            <div className="row py-4">
-              <div className="col-2"></div>
-              <div className="col-4">
-                <h3>Sean</h3>
-              </div>
-              <div className="col-4">
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/mediaicon.png`}
-                ></img>
-              </div>
-              <div className="col-2"></div>
+            <div className="col-2"></div>
+          </div>
+          <div className="row py-4">
+            <div className="col-2"></div>
+            <div className="col-4">
+              <img
+                alt="Head shot of Carolani"
+                style={{ height: "310px", width: "220px" }}
+                src={`${process.env.PUBLIC_URL}/images/carolani.jpg`}
+              ></img>
+            </div>
+            <div className="col-4">
+              <h3>Carolani Bartell - Writer</h3>
+              <p className="text-left">
+                Carolani is a writer from the DMV area. She has been working in
+                the entertainment industry since childhood, recently writing for
+                video games, internet content, and wonderful projects like
+                these. She also writes and edits fiction, music, and
+                screenplays, finding inspiration in just about any creative
+                outlet. Her passion for storytelling is often accompanied by
+                early mornings, a record player, and iced coffee.
+              </p>
+            </div>
+            <div className="col-2"></div>
+          </div>
+          <h2 className="py-5">Recorded Story</h2>
+          <div className="row py-4">
+            <div className="col-2"></div>
+            <div className="col-4">
+              <h3>Joseph Schmidt - Writer</h3>
+              <p className="text-right">
+                Joseph has always been fascinated by storytelling. Joseph is
+                currently honing his craft in South Dakota. When not writing he
+                enjoys induging in film, is a tech nerd, and loves to read.
+              </p>
+            </div>
+            <div className="col-4">
+              <img
+                alt="Head shot of Joseph"
+                style={{ height: "310px", width: "180px" }}
+                src={`${process.env.PUBLIC_URL}/images/joseph.jpg`}
+              ></img>
             </div>
           </div>
+          <br></br>
+          <br></br>
+          <br></br>
         </div>
       </div>
-      <br></br>
-      <br></br>
-      <br></br>
       <Footer></Footer>
     </>
   );
